ec75d5402ef3127009073e89bc1164276d68307b
jfilya <95137071+jfilya@users.noreply.github.com>
2022-02-19 00:36:13 +0300

    Toxin

--- js/main.js
+++ js/main.js
@@ -3,14 +3,11 @@ const wind = document.querySelector('.dropdown');
 const plus1 = document.getElementById('plus-1');
 const minus1 = document.getElementById('minus-1');
 let num1 =document.getElementById('numb-1');
-
+let num=0;
 const activeWindiw = () =>{
 dropFriend.addEventListener('click', () => {
     wind.classList.toggle('freand-active');
 });
 };
 activeWindiw();
-
-
-
 
