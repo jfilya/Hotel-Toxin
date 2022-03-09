c5bcb0171129eabcaaefdfd5be1ac210c09cecbf
jfilya <julia.94@bk.ru>
2022-03-09 21:57:33 +0300

    new

--- js/comfort.js
+++ js/comfort.js
@@ -1,8 +1,10 @@
 const comfortTitle = document.querySelector('.homedynamic__title')
 const dinamic = document.querySelector('.homedynamic__dinam')
+const arrow = document.querySelector('.homedynamic__title__arrow')
 const activeDinam = () => {
     comfortTitle.addEventListener("click", () => {
         dinamic.classList.toggle("freand-active");
+        arrow.classList.toggle("rotate-arrow");
   });
 };
 activeDinam();
