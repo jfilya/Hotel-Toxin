86e9a8e95ab210f5b9d2edf1c0afff5d033df8c9
jfilya <julia.94@bk.ru>
2022-03-10 20:56:08 +0300

    finish

--- js/check.js
+++ js/check.js
@@ -1,31 +1,33 @@
-// $('.minus').click(function () { // вызов функции при клике на элемент
-//     var $input = $(this).parent().find('input'); // объявление переменной и присвоение ей значения поля ввода
-//     var count = parseInt($input.val()) - 1; // объявление переменной, преобразование значения в число и уменьшение его на 1
-//     count = count<= 0 ? 0 : count; // условие, которое говорит "Если значение переменной меньше 0 то запиываем 0"
-//     $input.val(count); // присвоение атрибута value переменной count переменной $input
-
-// });
-// $('.plus').click(function () { // вызов функции при клике на элемент
-//     var $input = $(this).parent().find('input'); // объявление переменной и присвоение ей значения поля ввода
-//     $input.val(parseInt($input.val()) + 1); // объявление переменной, преобразование значения в число и увеличение его на 1
-// });
-
-
 
 $(document).ready(function() {
-    $('.minus').click(function () {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 0 ? 0 : count;
-        $input.val(count);
-        $input.change();
-        return false;
-    });
-    $('.plus').click(function () {
-        var $input = $(this).parent().find('input');
-        $input.val(parseInt($input.val()) + 1);
-        $input.change();
-        return false;
-    });   
-});
 
+  $(".custom").iqDropdown({
+    
+    selectionText: "гость",
+    textPlural: "гостей",
+    onChange: function(id, count, totalItems) {
+      console.log(id, count, totalItems);
+    },
+    beforeDecrement: function(id, itemCount) {
+      if (id === "adult") {
+        return itemCount.adult > itemCount.infant;
+      }
+      return true;
+    },
+    beforeIncrement: function(id, itemCount) {
+      if (id === "infant") {
+        return itemCount.adult > itemCount.infant;
+      }
+      return true;
+    }
+  });
+
+  $(".custom-message").iqDropdown({
+    
+      selectionText: "удобств",
+      textPlural: "удобств",
+      onChange: function(id, count, totalItems) {
+        console.log(id, count, totalItems);
+      },
+    });
+});
